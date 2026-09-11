83462e42342c38207f9a7e34d7b918aa960bf4ea
ArchieGallagher <gallagherarchie689@gmail.com>
2026-09-11 19:27:24 +0100

    Making the example main python file.

---
+++ main.py
@@ -0,0 +1,11 @@
+# I need to include some imports + the associated venv files.
+
+
+def main():
+    print("Hello Wolrd!")
+
+
+while True:
+    main()
+
+# Example code is above just to pad out the file
