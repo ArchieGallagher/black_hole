db004ce5c3c4c3e50a04202edad9b600d9002311
ArchieGallagher <gallagherarchie689@gmail.com>
2026-09-12 18:51:11 +0100

    Done with the python file...

--- main.py
+++ main.py
@@ -1,11 +1,90 @@
-# I need to include some imports + the associated venv files.
+import glfw
+import moderngl
+import numpy as np
+
+# This part is to help with making an FPS_calculator
+import time
+
+initial_time = time.perf_counter()
+frame = 0.0
 
 
-def main():
-    print("Hello Wolrd!")
+# Window
+if not glfw.init():
+    raise RuntimeError("Failed to initialize .glfw")
+
+glfw.window_hint(glfw.CONTEXT_VERSION_MAJOR, 3)
+glfw.window_hint(glfw.CONTEXT_VERSION_MINOR, 3)
+glfw.window_hint(glfw.OPENGL_PROFILE, glfw.OPENGL_CORE_PROFILE)
+
+width, height = 800, 600
+window = glfw.create_window(width, height, "Black Hole Sim", None, None)
+if not window:
+    glfw.terminate()
+    raise RuntimeError("Failed to create window")
+
+glfw.make_context_current(window)
+
+# Attaches to the context glfw just created
+ctx = moderngl.create_context()
 
 
-while True:
-    main()
+# LoadShaders from the file
+def load_shader(path):
+    with open(path, "r") as f:
+        return f.read()
 
-# Example code is above just to pad out the file
+
+vertex_shader = load_shader("shaders/vertex_shader.glsl")
+fragment_shader = load_shader("shaders/fragment_shader.glsl")
+
+program = ctx.program(vertex_shader=vertex_shader, fragment_shader=fragment_shader)
+
+quad_data = np.array(
+    [
+        # x,    y,    u,   v
+        -1.0,
+        -1.0,
+        0.0,
+        0.0,
+        1.0,
+        -1.0,
+        1.0,
+        0.0,
+        -1.0,
+        1.0,
+        0.0,
+        1.0,
+        1.0,
+        1.0,
+        1.0,
+        1.0,
+    ],
+    dtype="f4",
+)
+
+vbo = ctx.buffer(quad_data.tobytes())
+vao = ctx.vertex_array(program, [(vbo, "2f 2f", "in_position", "in_uv")])
+
+
+# Render loop
+while not glfw.window_should_close(window):
+    glfw.poll_events()
+
+    # This is part of the FPS_calculation
+    current_time = time.perf_counter()
+    time_elapsed = current_time - initial_time
+    if time_elapsed >= 1.0:
+        frames_per_second = frame / time_elapsed
+    frame += 1.0
+    window = glfw.set_window_title(window, f"Black Hole - {frames_per_second}")
+    ctx.clear(0.0, 0.0, 0.0)
+
+    # Update any per-frame uniforms here, e.g.:
+    # program['time'].value = glfw.get_time()
+
+    vao.render(moderngl.TRIANGLE_STRIP)
+
+    glfw.swap_buffers(window)
+
+glfw.terminate()
